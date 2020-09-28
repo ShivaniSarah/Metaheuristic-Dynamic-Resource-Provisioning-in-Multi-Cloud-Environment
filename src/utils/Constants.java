d68dc0643c5b852dd10ebadbd1d4443f5616906d
Shivani Agrawal <44539139+ShivaniSarah@users.noreply.github.com>
2020-09-28 22:28:13 +0530

    Add files via upload

---
+++ src/utils/Constants.java
@@ -0,0 +1,16 @@
+package cloudsample.utils;
+
+//import org.cloudbus.cloudsim.examples.utils.*;
+public class Constants {
+    
+    public static  int NO_OF_TASKS = 50;// NewJFrame1.a; // number of Cloudlets;
+    public static  int NO_OF_DATA_CENTERS = 5; // number of Datacenters;
+    public static  int VM_SIZE = 10000;
+    public static  int VM_RAM = 512;
+    public static  int VM_MIPS = 250;
+    public static  int VM_BW = 1000;
+    public static  int VM_CPUS= 1;
+    public static  int FILE_SIZE=100;
+    public static  int OUTPUT_SIZE=300;
+   
+}
